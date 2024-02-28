39a2588502d8a9694b35d9392ab3de7b104d3b8f
Baraxi21 <150504802+Baraxi21@users.noreply.github.com>
2024-02-28 15:11:03 +0400

    Add files via upload

--- test6.py
+++ test6.py
@@ -58,18 +58,6 @@ print(analysis_result['output'])
 # ]
 
 
-# def get_visualization(x,y,chart):
-#     """Get flight information between two locations."""
-
-#     # Example output returned from an API or database
-#     vis_info = {
-#         "chart-type": chart,
-#         "x": x,
-#         "y": y,
-#     }
-
-#     return json.dumps(vis_info)
-
 # analysis = f"Based on this {analysis_result['output']}, You need to answer to the user prompt accordingly"
 
 completion = openai.chat.completions.create(
@@ -103,4 +91,67 @@ completion = openai.chat.completions.create(
         function_call='auto'
     ) # specify the function call
 
-print(completion.choices[0].message.function_call.arguments)
+output = completion.choices[0].message.function_call.arguments
+print(output)
+
+def get_visualization(x,y,chart):
+
+    # Example output returned from an API or database
+    vis_info = {
+        "chart": chart,
+        "x": x,
+        "y": y,
+    }
+
+    return json.dumps(vis_info)
+
+chart_type = json.loads(output.function_call.arguments).get("chart")
+x_value = json.loads(output.function_call.arguments).get("x")
+y_value = json.loads(output.function_call.arguments).get("y")
+
+params = json.loads(output.function_call.arguments)
+type(params)
+
+print(chart_type)
+print(x_value)
+print(y_value)
+print(params)
+
+chosen_function = eval(output.function_call.name)
+viz = chosen_function(**params)
+
+print(viz)
+
+second_completion = openai.chat.completions.create(
+    model="gpt-3.5-turbo-0613",
+    messages=[
+        {"role": "user", "content": analysis_result},
+        {"role": "function", "name": output.function_call.name, "content": viz},
+    ],
+    functions=[
+        {
+        "name": "get_visualization",
+        "description": "Get parameters required to create a visualisation",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "chart": {
+                    "type": "string",
+                    "description": "The chart-type to create visualisation eg: bar, pie, scatter plot etc.",
+                },
+                "x": {
+                    "type": "string",
+                    "description": "The Column Names that need to be present in x-axis should be in a list of strings showing all column names",
+                },
+                "y": {
+                    "type": "integer",
+                    "description": "Column values that need to be present in y-axis corresponding to the Column Names in x-axis should be in a list of integers",
+                }
+            },
+            "required": ["chart-type", "x", "y"],
+        },
+    }
+        ],
+)
+response = second_completion.choices[0].message.content
+print(response)
